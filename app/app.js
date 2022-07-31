9902dd0c8dac8d4f92fbae217111b12de3551143
D3N2-A <anmolsharma999937@gmail.com>
2022-07-31 20:29:29 +0530

    message animation

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 const socket = io("ws://localhost:8080");
 
 socket.on("message", (text) => {
+  const d = new Date();
   const el = document.createElement("li");
+  const p = document.createElement("p");
   el.innerHTML = text;
+  p.innerHTML = d.toLocaleTimeString();
   document.querySelector("ul").appendChild(el);
+  document.querySelector("ul").appendChild(p);
 });
 
 document.querySelector("button").onclick = () => {
