434e30f5c82ee6ef260a6116a70c3bc9ec8dcbb1
D3N2-A <anmolsharma999937@gmail.com>
2022-08-04 09:21:56 +0530

    scroll fixed

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const socket = io("ws://localhost:8080");
 const blip = new Audio("./blip.wav");
+const messageBody = document.getElementById("mssg");
 socket.on("message", (text) => {
   const d = new Date();
   const el = document.createElement("li");
@@ -8,11 +9,13 @@ socket.on("message", (text) => {
   p.innerHTML = d.toLocaleTimeString();
   document.querySelector("ul").appendChild(el);
   document.querySelector("ul").appendChild(p);
+  messageBody.scrollTop = messageBody.scrollHeight;
 });
-
-document.querySelector("button").onclick = () => {
+document.querySelector("button").onclick = (e) => {
+  e.preventDefault();
   blip.play();
   const text = document.querySelector("input").value;
   socket.emit("message", text);
-  
+  const el = document.getElementById("ip");
+  el.value = "";
 };
